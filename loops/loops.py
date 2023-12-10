d17573bb2766b1fe4d762023e10a70d7a5785e0e
RadosavPanic <radosav.panic01@gmail.com>
2023-12-10 14:00:27 +0100

    working with range and loops

---
+++ loops/loops.py
@@ -0,0 +1,14 @@
+# Range, for loop, while loop
+
+# range(start, stop, step)
+numbers_list1 = list(range(1, 15, 2))  # [1, 3, 5, 7, 9, 11, 13]
+print(numbers_list1)
+
+# For loop with range(stop)
+for num in range(5):
+    print(num)  # 0 1 2 3 4
+
+# Foor loop with strings
+for letter in 'PythonX':
+    print(letter)  # P y t h o n X
+
