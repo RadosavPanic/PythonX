80e028f9e9f69f739686579ebe40e8966c8566ab
RadosavPanic <radosav.panic01@gmail.com>
2023-12-10 14:15:13 +0100

    working with while loop and break control statement

--- loops/loops.py
+++ loops/loops.py
@@ -12,3 +12,25 @@ for num in range(5):
 for letter in 'PythonX':
     print(letter)  # P y t h o n X
 
+# While loop -> while [boolean]
+counter = 0
+while counter < 3:
+    print(f"{counter + 1}: Python is great language")
+    counter = counter + 1  # will be printed 3 times (0, 1, 2) until 3
+
+# Break (loop control statement)
+for letter in 'PythonX':
+    if letter == 'o':
+        break
+    else:
+        print(letter)  # P y t h
+
+# Break statement when 50% capacity is reached, otherwise incremented by step of 10
+boat_capacity = 500
+treshold = boat_capacity * (50.0 / 100.0)
+for i in range(0, boat_capacity, 10):
+    if i > treshold:
+        print("50% capacity reached")
+        break
+    else:
+        print(f"Passengers onboarded: {i}")
